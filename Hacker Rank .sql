1a2251afe0233479b4cd5b75ec699de98be01c05
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-06-29 13:09:57 +0530

    Update Hacker Rank .sql

--- Hacker Rank .sql
+++ Hacker Rank .sql
@@ -1,25 +1,35 @@
 Difficultly Level Easy
 
+ 
+Revising the Select Query I:
 (1). Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
 select * from city
 where population > 100000 and 
 countrycode = 'usa'; 
 
+
+Revising the Select Query II:
 (2). Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 
 select name from city 
 where population > 120000 
 and countrycode = 'usa';
 
+
+Select All Query:
 (3). Query all columns (attributes) for every row in the CITY table.
 
  select * from city;
 
-(4). Query all columns for a city in CITY with the ID 1661.
 
+Select by Id:
+(4). Query all columns for a city in CITY with the ID 1661.
+ 
 select * from city where id='1661';
 
+
+Japanese Cities Attributes:
 (5). Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 
  select * from city where countrycode = 'JPN'; 
